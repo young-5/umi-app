d2cd3064c52ed3eccd39adbc5e6d2a6d2f88143b
young-5 <1552153802@qq.com>
2021-01-18 18:05:30 +0800

    fix(proxy.ts):代理修复

--- config/proxy.ts
+++ config/proxy.ts
@@ -4,7 +4,7 @@
  * File Created: 2020-12-16 13:33:05
  * Author: yangwenwu
  * ------
- * Last Modified: 2020-12-25 09:01:19
+ * Last Modified: 2021-01-18 18:04:45
  * Modified By: yangwenwu at <1552153802@qq.com>
  * ------
  * Copyright 2020 - Present, Your Company
@@ -12,16 +12,14 @@
 const HOST_URL = '';
 const BASE_IP = 'http://192.168.21.159:8081/'; //基础支撑平台swaggerUI
 export default {
-  proxy: {
-    '/api': {
-      target: HOST_URL,
-      changeOrigin: true,
-      pathRewrite: { '/api': '' },
-    },
-    '/base': {
-      target: BASE_IP,
-      changeOrigin: true,
-      pathRewrite: { '/base': '' },
-    },
+  '/api': {
+    target: HOST_URL,
+    changeOrigin: true,
+    pathRewrite: { '/api': '' },
+  },
+  '/base': {
+    target: BASE_IP,
+    changeOrigin: true,
+    pathRewrite: { '/base': '' },
   },
 };
--- src/components/componentsTools/ModalCustom/index.tsx
+++ src/components/componentsTools/ModalCustom/index.tsx
@@ -4,46 +4,46 @@
  * File Created: 2020-09-30 15:38:13
  * Author: yangwenwu
  * ------
- * Last Modified: 2020-11-20 09:00:34
+ * Last Modified: 2021-01-18 12:34:55
  * Modified By: yangwenwu at <1552153802@qq.com>
  * ------
  * Copyright 2020 - Present, Your Company
  */
-import ReactDom from 'react-dom'
+import ReactDom from 'react-dom';
 
 /** 自定义弹窗工具 */
 export default class ModalCustom {
-    /** 弹层DOM的id属性值 */
-    private modalId: string
-    /** 弹层的DOM元素 */
-    private modalEle: HTMLElement | null = null
+  /** 弹层DOM的id属性值 */
+  private modalId: string;
+  /** 弹层的DOM元素 */
+  private modalEle: HTMLElement | null = null;
 
-    constructor(modalId: string = 'react-admin_modal') {
-        this.modalId = modalId
-        this.initDom()
-    }
+  constructor(modalId: string = 'react-admin_modal') {
+    this.modalId = modalId;
+    this.initDom();
+  }
 
-    initDom() {
-        this.modalEle = document.getElementById(this.modalId)
-        if (!this.modalEle) {
-            this.modalEle = document.createElement('div')
-            this.modalEle.setAttribute('id', 'react-admin_modal-' + Math.random())
-            document.body.appendChild(this.modalEle)
-        }
+  initDom() {
+    this.modalEle = document.getElementById(this.modalId);
+    if (!this.modalEle) {
+      this.modalEle = document.createElement('div');
+      this.modalEle.setAttribute('id', 'react-admin_modal-' + Math.random());
+      document.body.appendChild(this.modalEle);
     }
+  }
 
-    /** 显示 */
-    show(component: React.ReactElement) {
-        if (!this.modalEle) this.initDom()
-        ReactDom.render(component, this.modalEle)
-    }
+  /** 显示 */
+  show(component: React.ReactElement) {
+    if (!this.modalEle) this.initDom();
+    ReactDom.render(component, this.modalEle);
+  }
 
-    /** 销毁 */
-    destroy() {
-        if (this.modalEle) {
-            ReactDom.unmountComponentAtNode(this.modalEle)
-            this.modalEle.parentNode!.removeChild(this.modalEle)
-            this.modalEle = null
-        }
+  /** 销毁 */
+  destroy() {
+    if (this.modalEle) {
+      ReactDom.unmountComponentAtNode(this.modalEle);
+      this.modalEle.parentNode!.removeChild(this.modalEle);
+      this.modalEle = null;
     }
+  }
 }
